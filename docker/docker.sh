40e63b4a9eb8c50c56d431f762ab346f6237e018
Arthur <arthur.wilcke@gmail.com>
2017-08-30 12:14:50 +0200

    made dockerfile more generic

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
+# get flags
+data_dir=''
+runs_dir=''
+
+while getopts 'd:r:' flag; do
+  case "${flag}" in
+    d) data_dir="${OPTARG}" ;;
+    r) runs_dir="${OPTARG}" ;;
+    *) error "Unexpected option ${flag}" ;;
+  esac
+done
+
+# get wd of main repo
+PWD=`pwd | rev | cut -d / -f2- | rev`
+
 nvidia-docker build -t "tf_classif" .
-nvidia-docker run -v /home/arthur:/home/arthur -v /nfs:/nfs -p 6006:6006 -ti tf_classif bash
+nvidia-docker run \
+    -v $PWD:/home \
+    -v $data_dir:/data \
+    -v $runs_dir:/runs \
+    -p 6006:6006 \
+    -ti tf_classif
